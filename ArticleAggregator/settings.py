b1b2ccc893dedfc355e4989de9f964b2fc116551
malovvan <malov-van@mail.ru>
2020-12-20 18:22:54 +0300

    Added Django REST Framework and API endpoint

--- ArticleAggregator/settings.py
+++ ArticleAggregator/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'articles',
+    'articles.apps.ArticlesConfig',
     'rest_framework',
+    'api.apps.ApiConfig',
+
 ]
 
 MIDDLEWARE = [
